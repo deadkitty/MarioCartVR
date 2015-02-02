f689e8bb1c630e5d098d0ea31de90413a6d8addb
Roman As <vanroman@web.de>
2015-02-02 15:26:50 +0100

    Preinclude Item Pictures

---
+++ Assets/Scripts/CartScripts/CartItemGui.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CartItemGui : MonoBehaviour 
+{
+
+	public int collecteditem;
+	private GameObject guimushroom;
+	private GameObject guishell;
+
+	// Use this for initialization
+	void Start () 
+	{
+		guimushroom = GameObject.Find ("guimushroom");
+		guishell = GameObject.Find ("guishell");
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	int cartitemgui (int collecteditem)
+	{
+
+		if (collecteditem == 1) 
+		{
+			 
+		}
+		if (collecteditem == 1) 
+		{
+		}
+		
+		int itemid = 0;
+		
+		return itemid;
+	}
+
+	void guiitemblink(int collecteditem)
+	{
+		item.SetActive (true);
+		yield return new WaitForSeconds(.1f);
+		item.SetActive (false);
+		yield return new WaitForSeconds(.1f);
+	}
+
+
+}
